a7314e604e81f097ed48be94b74b3dca572984cc
daviz1982 <daviz1982@gmail.com>
2021-07-14 15:52:39 +0200

    Test no localstorage (#2)
    * add search query and logs
    
    * fix bugs and improve search api
    
    * remove console logs
    
    Co-authored-by: dcruz <dcruz.c@gmail.com>

--- public/api/api.js
+++ public/api/api.js
@@ -2,27 +2,41 @@ const needle = require('needle')
 
 const search = async (searchTerm, callback) => {
   let feed, error
+  const { type } = getSearchType({ str: searchTerm })
   try {
-    // TODO: add logic to perform search when it's an user and when isn't
     if (searchTerm.length > 0) {
-      // console.log(searchTerm)
-      const { userIds } = await getUserId({ usernames: searchTerm })
-      userIds.forEach(async (item) => {
-        feed = await searchTweets({ userId: item })
+      if (type === 'user') {
+        const usernames = searchTerm.slice(1)
+        const userProfile = await getUserId({ usernames })
+        feed = await searchTweetsByUser({ userId: userProfile.id })
+        callback({ userProfile, feed, error })
+      } else {
+        const query = searchTerm
+        feed = await searchTweets({ query })
         callback({ searchTerm, feed, error })
-      })
+      }
     }
   } catch (e) {
-    // console.log({ e })
+    console.error({ e })
     error = e
     callback({ searchTerm, feed, error })
   }
 }
 
+const getSearchType = ({ str }) => {
+  const reUser = /^@.*$/i
+  const reHashtag = /^#.*$/i
+
+  const type = reUser.test(str) ? 'user' : reHashtag.test(str) ? 'hash' : ''
+
+  return { type }
+}
+
 const getUserId = async ({ usernames }) => {
-  const endpointURL = 'https://api.twitter.com/2/users/by?usernames='
+  const endpointURL = 'https://api.twitter.com/2/users/by'
   const params = {
-    usernames: usernames.join(','),
+    usernames: typeof usernames === 'object' ? usernames.join(',') : usernames,
+    'user.fields': 'profile_image_url,url,name',
   }
   const res = await needle('get', endpointURL, params, {
     headers: {
@@ -30,9 +44,8 @@ const getUserId = async ({ usernames }) => {
     },
   })
   if (res.body && res.body.data) {
-    return { userIds: res.body.data.map((item) => item.id) }
+    return res.body.data[0]
   } else {
-    // console.log(res.body)
     const err = res.body.errors[0].message
       ? res.body.errors[0].message
       : `${res.body.errors[0].title}: ${res.body.errors[0].detail}`
@@ -41,12 +54,12 @@ const getUserId = async ({ usernames }) => {
   }
 }
 
-const searchTweets = async ({ userId }) => {
+const searchTweetsByUser = async ({ userId }) => {
   const endpointURL = `https://api.twitter.com/2/users/${userId}/tweets`
   const res = await needle(
     'get',
     endpointURL,
-    {},
+    { expansions: 'author_id' },
     {
       headers: {
         authorization: `Bearer ${process.env.BEARER_TOKEN}`,
@@ -65,6 +78,30 @@ const searchTweets = async ({ userId }) => {
   }
 }
 
+const searchTweets = async ({ query }) => {
+  const endpointUrl = 'https://api.twitter.com/2/tweets/search/recent'
+
+  const res = await needle(
+    'get',
+    endpointUrl,
+    { query },
+    {
+      headers: {
+        authorization: `Bearer ${process.env.BEARER_TOKEN}`,
+      },
+    }
+  )
+
+  if (res.body) {
+    const response = {
+      tweets: res.body.data,
+    }
+    return response
+  } else {
+    const err = res.body.errors[0].message
+    throw new Error(err)
+  }
+}
 const api = {
   search,
 }
--- public/electron-starter.js
+++ public/electron-starter.js
@@ -33,7 +33,12 @@ const createWindow = () => {
   mainWindow.loadURL('http://localhost:3000')
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
+
+  mainWindow.webContents.setWindowOpenHandler((params) => {
+    electron.shell.openExternal(params.url)
+    return { action: 'deny' }
+  })
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -51,8 +56,8 @@ const createWindow = () => {
 app.on('ready', async () => {
   if (!!searchTerm) {
     api.search([searchTerm], (feed, searchTerm) => {
-      mainWindow.webContents.send('message', feed)
-      mainWindow.webContents.send('twitter-account', searchTerm)
+      mainWindow.webContents.send('received-tweets', feed)
+      mainWindow.webContents.send('performed-query', searchTerm)
     })
   }
   createWindow()
@@ -77,7 +82,6 @@ app.on('activate', () => {
 })
 
 const cleanLocalStorage = () => {
-  console.log('Cleaning...')
   electron.session.defaultSession.clearStorageData({
     storages: ['localstorage'],
   })
@@ -86,13 +90,11 @@ const cleanLocalStorage = () => {
 electron.ipcMain.on('search', (event, arg) => {
   searchTerm = arg
   if (!!searchTerm && typeof arg === 'string') {
-    api.search([searchTerm], ({ feed, searchTerm, error }) => {
-      // console.log('.....')
-      // console.log({ feed, searchTerm, error })
-      // console.log('.....')
-      !!feed && mainWindow.webContents.send('message', feed)
-      !!searchTerm && mainWindow.webContents.send('twitter-account', searchTerm)
-      !!error && mainWindow.webContents.send('error', error)
+    api.search(searchTerm, ({ feed, userProfile, error }) => {
+      !!feed && mainWindow.webContents.send('received-tweets', feed)
+      !!userProfile &&
+        mainWindow.webContents.send('received-user-profile', userProfile)
+      !!error && mainWindow.webContents.send('received-error', error)
     })
   }
 })
