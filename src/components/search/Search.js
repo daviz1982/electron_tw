8891e546ff14ba799dd5e527a20ee2ccd0f4bcc6
dcruz <dcruz.c@gmail.com>
2021-07-16 14:02:19 +0200

    set autofocus on search input

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,8 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import './Search.css'
 
 export const Search = ({ handleSubmitSearch }) => {
-  const [searchTerm, setSearchTerm] = useState()
+  const inputSearch = useRef(null)
+  const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    if (inputSearch.current) {
+      inputSearch.current.focus()
+    }
+  }, [])
 
   const handleChangeSearch = (e) => {
     setSearchTerm(e.target.value)
@@ -18,16 +25,18 @@ export const Search = ({ handleSubmitSearch }) => {
       <form onSubmit={handleSubmit}>
         <label htmlFor='searchfield'>Enter a topic to search</label>
         <input
-          type='search'
+          autoFocus
           id='searchfield'
-          placeholder=''
           onChange={handleChangeSearch}
+          placeholder=''
+          type='text'
+          ref={inputSearch}
           value={searchTerm}
         />
-        {/* <small>
+        <small>
           <div>use @ to look for users</div>
           <div>use # to look for hashtags</div>
-        </small> */}
+        </small>
       </form>
     </div>
   )
